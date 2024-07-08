aac2d1a6a641389d6c6d61faa6286ae80c8bc055
Johnn <johnndavid27@gmail.com>
2024-07-08 20:01:55 -0300

    atualizando vendas

--- vendas/models.py
+++ vendas/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from usuarios.models import Barbeiro
 from estoque.models import Produto
 from agendamentos.models import Agendamento
+from decimal import Decimal
 
 class Venda(models.Model):
     agendamento = models.OneToOneField(Agendamento, on_delete=models.CASCADE)
@@ -21,17 +22,36 @@ class Venda(models.Model):
         ('Pix', 'Pix'),
     ])
     
-
     def calcular_valor_total(self):
-        total = sum([vp.produto.preco_venda * vp.quantidade for vp in self.vendaproduto_set.all()])
+        print("Calculando valor total...")
+        total = Decimal('0.00')  # Inicializa o valor total como Decimal
+        for vp in self.vendaproduto_set.all():
+            produto_total = vp.produto.preco_venda * vp.quantidade
+            total += produto_total
+            print(f"Adicionando produto: {vp.produto.nome}, Quantidade: {vp.quantidade}, Preço Unitário: {vp.produto.preco_venda}, Total do Produto: {produto_total}")
+        
+        if self.agendamento.servico:
+            total += self.agendamento.servico.preco  # Adiciona o valor do serviço ao valor total
+            print(f"Adicionando serviço: {self.agendamento.servico.nome}, Preço: {self.agendamento.servico.preco}")
+        
         if self.desconto:
+            print(f"Aplicando desconto: {self.desconto}")
             total -= self.desconto
-        self.valor_total = total
+        
+        print(f"Valor total calculado: {total}")
+        self.valor_total = total  # Atribui o valor total calculado
         self.save()
+        print("Valor total salvo no banco de dados")
+    # def calcular_valor_total(self):
+    #     total = sum([vp.produto.preco_venda * vp.quantidade for vp in self.vendaproduto_set.all()])
+    #     if self.desconto:
+    #         total -= self.desconto
+    #     self.valor_total = total
+    #     self.save()
 
-    def calcular_comissao_barbeiro(self):
-        comissao_valor = self.valor_total * (self.barbeiro.comissao_porcentagem / 100)
-        return comissao_valor
+    # def calcular_comissao_barbeiro(self):
+    #     comissao_valor = self.valor_total * (self.barbeiro.comissao_porcentagem / 100)
+    #     return comissao_valor
 
 
         # def calcular_valor_total(self):
--- vendas/views.py
+++ vendas/views.py
@@ -4,6 +4,8 @@ from agendamentos.models import Agendamento
 from estoque.models import Produto
 from usuarios.models import Barbeiro
 from django.contrib import messages
+from django.urls import reverse
+from decimal import Decimal
 
 def lista_vendas(request):
     vendas = Venda.objects.filter(status_pagamento='Em Aberto')
@@ -14,47 +16,124 @@ def detalhes_venda(request, pk):
     return render(request, 'detalhes_venda.html', {'venda': venda})
 
 
-
 def criar_venda(request):
     if request.method == 'POST':
+        print("Recebendo dados do formulário POST")
         agendamento_id = request.POST.get('agendamento')
         produtos_ids = request.POST.getlist('produtos')
         barbeiro_id = request.POST.get('barbeiro')
-        desconto = request.POST.get('desconto', 0)
+        desconto = request.POST.get('desconto', '0')
+        forma_pagamento = request.POST.get('forma_pagamento')
+
+        print(f"Agendamento ID: {agendamento_id}")
+        print(f"Produtos IDs: {produtos_ids}")
+        print(f"Barbeiro ID: {barbeiro_id}")
+        print(f"Desconto: {desconto}")
+        print(f"Forma de Pagamento: {forma_pagamento}")
 
         try:
             agendamento = get_object_or_404(Agendamento, pk=agendamento_id)
-            barbeiro = get_object_or_404(Barbeiro, pk=barbeiro_id)
+            print(f"Agendamento: {agendamento}")
 
-            venda = Venda.objects.create(
+            if not desconto:
+                desconto = Decimal('0.00')
+                print('Desconto não aplicado')
+            else:
+                desconto = Decimal(desconto)  # Convertendo desconto para Decimal
+                print('Desconto aplicado')
+
+            venda, created = Venda.objects.get_or_create(
                 agendamento=agendamento,
-                desconto=desconto,
-                barbeiro=barbeiro,
-                valor_comissao=0
+                defaults={
+                    'desconto': desconto,
+                    'barbeiro': get_object_or_404(Barbeiro, pk=barbeiro_id),
+                    'valor_total': Decimal('0.00'),
+                    'forma_pagamento': forma_pagamento
+                }
             )
-            venda.valor_total = 0
-            
+
+            if not created:
+                print("Venda existente encontrada. Atualizando venda existente.")
+                venda.desconto = desconto
+                venda.forma_pagamento = forma_pagamento
+                venda.save()
+            else:
+                print(f"Nova venda criada: {venda}")
+
+            # Adiciona o valor do serviço ao valor total da venda
             for produto_id in produtos_ids:
                 produto = get_object_or_404(Produto, pk=produto_id)
-                quantidade = int(request.POST.get(f'quantidade_{produto_id}'))
+                quantidade = int(request.POST.get(f'quantidade_{produto_id}', 1))
+                print(f"Produto: {produto} - Quantidade: {quantidade}")
                 VendaProduto.objects.create(venda=venda, produto=produto, quantidade=quantidade)
-                venda.valor_total += produto.preco * quantidade
+                print(f"Produto adicionado: {produto}, Quantidade: {quantidade}")
 
-            venda.calcular_valor_total()
-            venda.save()
-            return redirect('vendas:vendas')
+            venda.calcular_valor_total()  # Calcula o valor total da venda
+            
+            print(f"Valor total da venda após cálculo: {venda.valor_total}")
+
+            return redirect(reverse('vendas:lista_vendas'))
 
         except Exception as e:
+            print(f"Erro: {str(e)}")
             messages.error(request, str(e))
 
-    # Para depuração
     agendamentos = Agendamento.objects.filter(status='pendente')
     if not agendamentos.exists():
+        print("Nenhum agendamento pendente encontrado.")
         messages.error(request, 'Nenhum agendamento pendente encontrado.')
 
     produtos = Produto.objects.all()
     barbeiros = Barbeiro.objects.all()
     return render(request, 'criar_venda.html', {'agendamentos': agendamentos, 'produtos': produtos, 'barbeiros': barbeiros})
+
+# def criar_venda(request):
+#     if request.method == 'POST':
+#         agendamento_id = request.POST.get('agendamento')
+#         produtos_ids = request.POST.getlist('produtos')
+#         barbeiro_id = request.POST.get('barbeiro')
+#         desconto = request.POST.get('desconto', '0')
+#         forma_pagamento = request.POST.get('forma_pagamento')
+
+#         try:
+#             agendamento = get_object_or_404(Agendamento, pk=agendamento_id)
+#             barbeiro = get_object_or_404(Barbeiro, pk=barbeiro_id)
+
+            
+#             if not desconto:
+#                 desconto = 0.0
+#             else:
+#                 desconto = float(desconto)
+
+#             venda = Venda.objects.create(
+#                 agendamento=agendamento,
+#                 desconto=desconto,
+#                 barbeiro=barbeiro,
+#                 valor_comissao=0,
+#                 forma_pagamento=forma_pagamento
+#             )
+#             venda.valor_total = 0
+            
+#             for produto_id in produtos_ids:
+#                 produto = get_object_or_404(Produto, pk=produto_id)
+#                 quantidade = int(request.POST.get(f'quantidade_{produto_id}'))
+#                 VendaProduto.objects.create(venda=venda, produto=produto, quantidade=quantidade)
+#                 venda.valor_total += produto.preco * quantidade
+
+#             venda.calcular_valor_total()
+#             venda.save()
+#             return redirect('vendas:vendas')
+
+#         except Exception as e:
+#             messages.error(request, str(e))
+
+#     agendamentos = Agendamento.objects.filter(status='pendente')
+#     if not agendamentos.exists():
+#         messages.error(request, 'Nenhum agendamento pendente encontrado.')
+
+#     produtos = Produto.objects.all()
+#     barbeiros = Barbeiro.objects.all()
+#     return render(request, 'criar_venda.html', {'agendamentos': agendamentos, 'produtos': produtos, 'barbeiros': barbeiros})
 
 # def criar_venda(request):
 #     if request.method == 'POST':
